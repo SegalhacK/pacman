79dfb0c9a89ef37daba6b9a5de5a1a34928f30c1
Christian Manriquez <chrmanriquez@gmail.com>
2021-01-03 18:36:40 -0300

    [ADD] movimientos pocman y deteccion de paredes. Trabajar en fantasmas, game over y contador de vidas

--- code.js
+++ code.js
@@ -3,51 +3,90 @@
 2) que el pacman se mueva*/
 
 var world = [
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2],
-    [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2],
-    [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2],
-    [2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 1, 2],
-    [2, 1, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2],
-    [2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2],
-    [2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 1, 2],
-    [2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2],
-    [2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2],
-    [2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2],
-    [2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 2, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+  [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+  [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2],
+  [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2],
+  [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2],
+  [2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2],
+  [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2],
+  [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+  [2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 1, 2],
+  [2, 1, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2],
+  [2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
 ];
 
+var score = 0;
+
+var pacman = {
+  x: 1,
+  y: 1
+};
+
 function displayWorld() {
-    var output = '';
-    for (i = 0; i < world.length; i++) {
-        output += "<div class='row'>";
-        for (var j = 0; j < world[i].length; j++) {
-            if (world[i][j] == 2)
-                output += "<div class='brick'></div>";
-            else if (world[i][j] == 1)
-                output += "<div class='coin'></div>";
-            if (world[i][j] == 0)
-                output += "<div class='empty'></div>";
-        }
-        output += "</div>";
+  var output = "";
+  for (i = 0; i < world.length; i++) {
+    output += "<div class='row'>";
+    for (var j = 0; j < world[i].length; j++) {
+      if (world[i][j] == 2) {
+        output += "<div class='brick'></div>";
+      } else if (world[i][j] == 1) {
+        output += "<div class='coin'></div>";
+      } else if (world[i][j] == 0) {
+        output += "<div class='empty'></div>";
+      }
     }
-    //console.log(output);
-    document.getElementById('world').innerHTML = output;
+    output += "</div>";
+  }
+  //console.log(output);
+  document.getElementById('world').innerHTML = output;
+}
+
+function displayPacman() {
+  document.getElementById('pacman').style.top = pacman.y * 30 + "px";
+  document.getElementById('pacman').style.left = pacman.x * 30 + "px";
+}
+function displayScore() {
+  document.getElementById('score').innerHTML = score;
 }
 
 displayWorld();
+displayPacman();
+displayScore();
+
+document.onkeydown = function (e) {
+  if (e.keyCode == 37 && world[pacman.y][pacman.x - 1] != 2) { //izquierda
+    document.getElementById('pacman').style.transform = "scaleX(-1)";
+    pacman.x--;
+  }
+  else if (e.keyCode == 39 && world[pacman.y][pacman.x + 1] != 2) { //derecha
+    document.getElementById('pacman').style.transform = "rotate(0deg)";
+    pacman.x++;
+  }
+  else if (e.keyCode == 38 && world[pacman.y - 1][pacman.x] != 2) { //arriba
+    document.getElementById('pacman').style.transform = "rotate(270deg)";
+    pacman.y--;
+  }
+  else if (e.keyCode == 40 && world[pacman.y + 1][pacman.x] != 2) { //abajo
+
+    document.getElementById('pacman').style.transform = "rotate(90deg)";
+    pacman.y++;
+  }
+
+  if (world[pacman.y][pacman.x] == 1) {
+    world[pacman.y][pacman.x] = 0;
+    score += 10;
+    displayWorld();
+    displayScore();
+  }
+  if (world[pacman.y][pacman.x] == 3) {
+    world[pacman.y][pacman.x] = 0;
+    score += 50;
+    displayWorld();
+    displayScore();
+  }
+  // console.log(e.keyCode);
+  displayPacman();
+}
